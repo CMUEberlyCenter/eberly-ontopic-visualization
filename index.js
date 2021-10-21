f8e8f23e9d32b2df2182c26a7c240c8d6ab4df66
Martin van Velsen <vvelsen@gmail.com>
2021-10-21 12:18:57 -0400

    Already published the barebones library framework to npmjs. Now we're going to populate it with the skeleton from our react npm library project

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+const magicString = () => {
+    return 'Import/Export Test';
+};
+
+/**
+ *
+ */
+const displayMyName = (name) => {
+    return `Your name is ${name}`;
+};
+
+module.exports = {
+    magicString,
+    displayMyName
+};
